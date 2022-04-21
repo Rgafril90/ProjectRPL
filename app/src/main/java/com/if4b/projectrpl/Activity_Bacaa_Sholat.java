495d34f71f3e1472c4762f9b4e0578e23d89a0c2
Rgafril90 <93838735+Rgafril90@users.noreply.github.com>
2022-04-21 22:46:47 +0700

    Main.xml

---
+++ app/src/main/java/com/if4b/projectrpl/Activity_Bacaa_Sholat.java
@@ -0,0 +1,14 @@
+package com.if4b.projectrpl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Bacaa_Sholat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bacaa_sholat);
+    }
+}
---
+++ app/src/main/java/com/if4b/projectrpl/Activity_Bacaan_Ayat_Kursi.java
@@ -0,0 +1,14 @@
+package com.if4b.projectrpl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Bacaan_Ayat_Kursi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bacaan_ayat_kursi);
+    }
+}
